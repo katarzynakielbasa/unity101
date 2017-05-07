8824c71f3a6ff800095c07874f8d1c7b4bc88167
Katarzyna Kiełbasa <katarzyna.kielbasa88@gmail.com>
2017-05-07 03:43:24 +0200

    - added assets - altered shaders - implemented movement of platforms - added coroutine for rock movement - refactored code to use inheritance for rock object - added zombie animations (idle, jump) - added physics - added audio (game audio, zombie jump, zombie death) - added collision detections

---
+++ Hunted Zombie Rush/Assets/Scripts/PlatformObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformObject : MonoBehaviour {
+
+    [SerializeField] private float objectSpeed = 1;
+    [SerializeField] private float resetPosition = 4.6f;
+    [SerializeField] private float startPosition = -131.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        transform.Translate(Vector3.right * (objectSpeed * Time.deltaTime));
+
+        if(transform.localPosition.x >= resetPosition) {
+            Vector3 newPosition = new Vector3(startPosition, transform.position.y, transform.position.z);
+            transform.position = newPosition;
+        }
+	}
+}
---
+++ Hunted Zombie Rush/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class Player : MonoBehaviour {
+
+    private Animator anim;
+    private Rigidbody rigidbody;
+    [SerializeField] private float jumpForce = 100.0f;
+    [SerializeField] private AudioClip sfxJump;
+    [SerializeField] private AudioClip sfxDeath;
+
+    private bool jump = false;
+    private AudioSource audioSource;
+
+    void Awake() {
+        Assert.IsNotNull(sfxJump);
+        Assert.IsNotNull(sfxDeath);
+    }
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+            Debug.Log("Jump");
+            anim.Play("Jump");
+            audioSource.PlayOneShot(sfxJump);
+            rigidbody.useGravity = true;
+            jump = true;
+        }
+	}
+    private void FixedUpdate() {
+        if (jump == true) {
+            jump = false;
+
+            rigidbody.velocity = new Vector2(0, 0);
+            rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode.Impulse);
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "obstacle") {
+            rigidbody.AddForce(new Vector2(50, 20), ForceMode.Impulse);
+            rigidbody.detectCollisions = false;
+            audioSource.PlayOneShot(sfxDeath);
+        }
+    }
+}
